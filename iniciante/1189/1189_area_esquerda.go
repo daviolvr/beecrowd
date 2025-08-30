d9c9c32ddcc75dab6fcb2dcb5c1e6737dcc50a91
Davi de Oliveira <olidavi031130@gmail.com>
2025-08-30 16:39:12 -0300

    1189, 1190 done

---
+++ iniciante/1189/1189_area_esquerda.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// condition: i > j and i + j < 11
+	var operation string
+	var matriz [12][12]float64
+	var soma float64
+	var count int
+
+	fmt.Scan(&operation)
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 12; j++ {
+			fmt.Scan(&matriz[i][j])
+		}
+	}
+
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 12; j++ {
+			if i > j && i+j < 11 {
+				soma += matriz[i][j]
+				count++
+			}
+		}
+	}
+
+	if operation == "S" {
+		fmt.Printf("%.1f\n", soma)
+	} else if operation == "M" {
+		media := soma / float64(count)
+		fmt.Printf("%.1f\n", media)
+	}
+}
